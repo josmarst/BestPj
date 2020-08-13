63109a9aeb3a189b3b1cb206fc379c80330fa7bf
josmarst <61290392+josmarst@users.noreply.github.com>
2020-08-12 23:35:22 -0300

    Add files via upload

---
+++ Models/Contato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebBPJ.Models
+{
+    public class Contato
+    {
+        [Key]
+        public int idContato { get; set; }
+
+
+        [Required(ErrorMessage = "O Nome é obrigatório", AllowEmptyStrings = false)]
+        public string Nome { get; set; }
+
+        public string Celular { get; set; }
+
+        [Required(ErrorMessage = "O Telefone é obrigatório", AllowEmptyStrings = false)]
+        [Display(Name = "Telefone Res.")]
+        public string TelResidencial { get; set; }
+    }
+}
